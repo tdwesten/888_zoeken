8137f5bef7a552f1cca907109625f3e044fe8dff
Thomas van der Westen <thomas@jannesmannes.nl>
2016-12-07 11:11:48 +0100

    Slack webhook added

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,23 @@
 var express = require( 'express' );
 var router = express.Router();
 const elastic = require( '../controllers/elasticsearch.ctrl' );
+var Slack = require( 'slack-node' );
+
+slack = new Slack();
+slack.setWebhook( process.env.SLACK_WEBHOOK );
+
 
 router.get( '/s/:input', function ( req, res ) {
     elastic.search( req.params.input )
         .then( function ( result ) {
+
+            slack.webhook( {
+                channel: "#888",
+                username: "888",
+                text: "Er is gezocht op: " + req.params.input + "!"
+            }, function ( err, response ) {
+            } );
+
             res.json( result.hits )
         } );
 
